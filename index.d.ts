f5c21cefa7fea9e44ede607c6f63d27441f82206
Yannick Lang <layaxx@gmx.de>
2022-08-14 19:42:55 +0200

    add types

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,17 @@
 // Type definitions
+import { AxiosRequestConfig } from "axios"
+import { X2jOptions } from "fast-xml-parser";
 
-export function read(url: string): Promise<FeedData>;
+export function read(url: string): Promise<FeedData | null>;
 
+export function getRequestOptions(): AxiosRequestConfig<any>;
+export function setRequestOptions(opts: AxiosRequestConfig<any>): void;
+
+export function getReaderOptions(): ReaderOptions;
+export function setReaderOptions(opts: Partial<ReaderOptions>): void;
+
+export function getParserOptions(): Partial<X2jOptions>
+export function setParserOptions(opts: Partial<X2jOptions>): void
 
 export interface FeedData {
     link?: string;
@@ -11,4 +21,10 @@ export interface FeedData {
     language?: string;
     published?: date;
     entries?: array;
+}
+
+export type ReaderOptions = {
+    descriptionMaxLen: number;
+    includeFullContent: boolean;
+    convertPubDateToISO: boolean;
 }
